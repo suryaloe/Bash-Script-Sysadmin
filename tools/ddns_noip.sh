a3b63a6c6c670df1d6d5dc1e56c8ad166ca2ae1c
Surya Loe <me.suryaloe@gmail.com>
2016-11-03 09:20:25 +0800

    ddns noip

---
+++ tools/ddns_noip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+hostname="hostname.ddns.net"
+user="usernoip"
+pass="passnoip"
+url="http://dynupdate.no-ip.com/nic/update?hostname=$hostname&myip="
+log="/var/log/ddns_noip"
+
+registered=$(nslookup $hostname|tail -n2|grep A|sed s/[^0-9.]//g)
+current=$(wget -q -O - http://ipinfo.io/ip)
+
+if [ "$current" != "$registered" ]
+then
+        update_dns=$(wget -q -O - "$url$current" --user=$user --password=$pass)
+        echo "DDNS Updated On: " `date` "IP : " $current > $log
+        echo "DDNS Updated On: " `date` "IP : " $current
+fi
